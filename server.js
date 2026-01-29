a2ff527906c02410e59a503bfe411f9eb93ee15c
Areli Perdue <perdue.areli28@gmail.com>
2026-01-29 15:01:41 -0600

    Server set up

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 // Importar el modulo HTTP
 const http = require('http');
 
+//file service
+const fs = require('fs');
+
 //Definir el puerto en que se ejecutara el servidor
 const PORT = 3000;
 
+//Crear el servidor
 const servidor = http.createServer((req, res) => {
-
-if (req.url === '/') {
+    //Lectura asincrona: No bloquea el Evento Loop.
+    fs.readFile('./index.html', (err, data) => {
+        if(err) {
+            res.statusCode = 500;
+            res.end('Error al leer el archivo');
+            return;
+        
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+        });
+/* if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });   
         res.end('<h1>Pagina principal\n</h1>');
 
@@ -17,7 +32,7 @@ if (req.url === '/') {
     else {
         res.writeHead(404, { 'Content-Type': 'text/html' });
         res.end('<i>Pagina no encontrada\n</i>');
-    }
+    } */
 });
 
 servidor.listen(PORT, () => {
